dac89b8e3807812d3fbfd1b46e7e289800ed46ff
sdole86 <92174592+sdole86@users.noreply.github.com>
2022-08-16 13:35:40 -0400

    * Added increasing difficulty. Snake speed increases with each point gained

---
+++ colorselection.py
@@ -0,0 +1,8 @@
+class ColorSelection:
+    white = (255, 255, 255)
+    yellow = (255, 255, 102)
+    black = (0, 0, 0)
+    red = (213, 50, 80)
+    green = (0, 255, 0)
+    blue = (50, 153, 213)
+
